1348b3da2baf683a4d8a1c580029e2e7e33a18cb
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-03-31 14:09:31 +0100

    pass in firebase arguments

--- pipelines/deploy.sh
+++ pipelines/deploy.sh
@@ -1,10 +1,11 @@
 npm install -g firebase-tools@10.8.0
+echo $1 $2 $3
 firebase \
     --debug \
     --config \
-    ../firebase.json deploy \
-    --token $(firebase.token) \
-    --project $(firebase.project) \
+    firebase.json deploy \
+    --token "$1" \
+    --project "$2" \
     --only functions \
-    --force -m "$(Build.buildNumber)" \
+    --force -m "$3" \
     --non-interactive
