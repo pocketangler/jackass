e638bb9a00fb716deef26740124429745cc8cc85
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-04-01 13:40:01 +0100

    run playwright with firebase emulators

--- test/e2e/playwright.config.js
+++ test/e2e/playwright.config.js
@@ -16,12 +16,11 @@ module.exports = defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -32,7 +31,7 @@ module.exports = defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
+  projects: process.env.CI ? [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
@@ -67,6 +66,11 @@ module.exports = defineConfig({
     //   name: 'Google Chrome',
     //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
+  ]: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
   ],
 
   /* Run your local dev server before starting the tests */
