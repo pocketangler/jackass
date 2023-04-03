e47f4d1a199aee743397f525142bed7267d63fb4
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-04-03 11:22:19 +0100

    refactored test logic

---
+++ test/e2e/fixtures/login-fixture.js
@@ -0,0 +1,50 @@
+import { expect } from "@playwright/test";
+
+export default async ({ page, context }, use) => {
+
+    await use({
+
+        async withUsernameAndPassword(username, password) {
+
+            await page.goto("/");
+            const storageState = await context.storageState();
+            if(!storageState.origins.some(o => o.localStorage.find(x => x.name === "dev-mode")))
+            {
+                await page.evaluate(() => localStorage.setItem("dev-mode", true));
+                await page.reload();
+            }
+            await page.getByText("Sign in").click();
+            await page.locator("form").getByText("Email").fill(username);
+            await page.locator("form").getByText("Password").fill(password);
+            await Promise.all([
+                page.waitForURL("/"),
+                page.locator("form").getByText("Sign In").click()
+            ]);
+
+        },
+
+        async expectSignedInAs(name) {
+
+            await page.waitForSelector("body.authenticated");
+            await expect(page.locator("aside .username")).toContainText(name);
+            await expect(page.locator("aside button", { hasText: "Sign out"})).toBeVisible();
+
+        },
+
+        async signOut() {
+
+            await page.click("button", { hasText: "Sign out" });
+
+        },
+
+        async expectSignedOut() {
+
+
+            await page.waitForSelector("body.authentication-initialised:not(.authenticated)");
+            await expect(page.locator("aside button", { hasText: "Sign in" })).toBeVisible();
+
+        }
+
+    });
+
+}
